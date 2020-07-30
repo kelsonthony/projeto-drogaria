bcb54059d4f7e7364ea7b92628fbec4fc7c4c836
kelsonthony <k.thony@gmail.com>
2020-07-30 18:48:10 -0300

    Jnit Test add
    Jnit Test add

---
+++ Drogaria/src/test/java/br/com/kelsonthony/drogaria/util/HibernateUtilTest.java
@@ -0,0 +1,14 @@
+package br.com.kelsonthony.drogaria.util;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+public class HibernateUtilTest {
+	
+	@Test
+	public void conectar() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.close();
+		HibernateUtil.getSessionFactory().close();
+	}
+}
