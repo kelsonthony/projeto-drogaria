5ff5b478c229a763fc086e5e0e3b06e6e9ab8afa
kelsonthony <k.thony@gmail.com>
2020-07-30 17:59:02 -0300

    Update hibernate and mysql setup
    Update hibernate and mysql setup

---
+++ Drogaria/src/main/java/br/com/kelsonthony/drogaria/main/HibernateUtilTest.java
@@ -0,0 +1,13 @@
+package br.com.kelsonthony.drogaria.main;
+
+import org.hibernate.Session;
+
+import br.com.kelsonthony.drogaria.util.HibernateUtil;
+
+public class HibernateUtilTest {
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.close();
+		HibernateUtil.getSessionFactory().close();
+	}
+}
